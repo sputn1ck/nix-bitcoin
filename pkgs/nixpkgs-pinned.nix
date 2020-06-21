5fd96beea4fb31194623be53d6555db441f74236
nixbitcoin <nixbitcoin@i2pmail.org>
2020-06-21 08:25:54 +0000

    nixpkgs: update stable and unstable

--- pkgs/nixpkgs-pinned.nix
+++ pkgs/nixpkgs-pinned.nix
@@ -8,11 +8,11 @@ in
 {
   # To update, run ../helper/fetch-channel REV
   nixpkgs = fetch {
-    rev = "7829e5791ba1f6e6dbddbb9b43dda72024dd2bd1";
-    sha256 = "0hs9swpz0kibjc8l3nx4m10kig1fcjiyy35qy2zgzm0a33pj114w";
+    rev = "788764b193f681a0d97c1b3e98cd687d131ef6b7";
+    sha256 = "1q3s901qcr42zwqiy94q35l07hiix0d5hsymib9a2kb0b8m75283";
   };
   nixpkgs-unstable = fetch {
-    rev = "8ba41a1e14961fe43523f29b8b39acb569b70e72";
-    sha256 = "0c2wn7si8vcx0yqwm92dpry8zqjglj9dfrvmww6ha6ihnjl6mfhh";
+    rev = "13e2c75c932adac6a198e35b04e2cb9a1eaf86cf";
+    sha256 = "0q1dcnclgzgv6sl0mynhn14nmb08g0cckd7zzyv2phzla18hap7a";
   };
 }
